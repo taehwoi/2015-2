5314631babb7b5cd4ac412543d2aeec55f7227b5
Thoum <indiofish@gmail.com>
2015-09-10 22:31:03 +0900

    finished c2add

--- pp/hw_1/crazy2add.rkt
+++ pp/hw_1/crazy2add.rkt
@@ -1,6 +1,6 @@
 #lang racket
-(define p0 '(z n . p))
-(define p1 '(n z . p))
+(define p0 '(z z z . p))
+(define p1 '(p . p))
 
 (define (val c)
   (cond ((equal? c 'z) 0)
@@ -14,24 +14,33 @@
     ;base case
     (val p)))
 
-(define (sum c0 c1) ;sum of a digit
-  (cond ((equal? (+ (val c0) (val c1)) 2) '(z . p))
-        ((equal? (+ (val c0) (val c1)) 1) '(p . z))
-        ((equal? (+ (val c0) (val c1)) 0) '(z . z))
-        ((equal? (+ (val c0) (val c1)) -1) '(n . z))
-        ((equal? (+ (val c0) (val c1)) -2) '(z . n))
-        ((equal? (+ (val c0) (val c1)) 3) '(p . p));needed?
-        ((equal? (+ (val c0) (val c1)) -3) '(n . n));needed?
-        ))
+(define (sum c0 c1 crry) ;sum of a digit
+  (cond ((equal? (+ (val c0) (val c1) (val crry)) 3) '(p . p))
+        ((equal? (+ (val c0) (val c1) (val crry)) 2) '(z . p))
+        ((equal? (+ (val c0) (val c1) (val crry)) 1) '(p . z))
+        ((equal? (+ (val c0) (val c1) (val crry)) 0) '(z . z))
+        ((equal? (+ (val c0) (val c1) (val crry)) -1) '(n . z))
+        ((equal? (+ (val c0) (val c1) (val crry)) -2) '(z . n))
+        ((equal? (+ (val c0) (val c1) (val crry)) -3) '(n . n))))
 
-;TODO : add carry as an argument?('z for start?) then sum gets three num also? or call sum twice?
-;add one digit each
-(define (c2add c0 c1)
-  (cond  ((and (not (pair? c0)) (not (pair? c1))) (sum c0 c1));check
-         ((and (not (pair? c0)) (pair? c1)) (cons (car (sum c0 (car c1))) (c2add (cdr (sum c0 (car c1))) (cdr c1))))
-         ((and (pair? c0) (not (pair? c1))) (cons (car (sum (car c0) c1)) (c2add (cdr c0) (cdr (sum (car c0) c1)))))
-         (else (cons (car (sum (car c0) (car c1))) (c2add (cdr c0) (cdr c1))))))
+(define (c2add c0 c1 crry)
+  (cond  ((and (not (pair? c0)) (not (pair? c1))) 
+          (sum c0 c1 crry));check
+         ((and (not (pair? c0)) (pair? c1))
+          (let ([p (sum c0 (car c1) crry)]) p
+            (cons (car p) (c2add (cdr p)) (cdr c1) 0)))
+         ((and (pair? c0) (not (pair? c1))) 
+          (let ([p (sum c1 (car c0) crry)]) p
+            (cons (car p) (c2add (cdr c0) (cdr p) 0))))
+         (else 
+           (let ([p (sum (car c1) (car c0) crry)]) p
+             (cons (car p) (c2add (cdr c0) (cdr c1) (cdr p)))))))
 
+(define (crazy2add c0 c1)
+  ;carry is 0 at the beginning
+  (c2add c0 c1 0))
 
-(c2add p0 p1)
-(crazy2val (c2add p0 p1))
+(+ 1 2 3)
+
+(crazy2add p0 p1)
+(crazy2val (crazy2add p0 p1))
