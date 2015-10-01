886a6e369c3ccb8126fc6dab03574d7b295d02da
Thoum <indiofish@gmail.com>
2015-10-01 23:58:58 +0900

    more abstraction

--- pp/hw1/hw1-4.rkt
+++ pp/hw1/hw1-4.rkt
@@ -1,28 +1,50 @@
 #lang racket
 (provide leaf node)
 (provide is-empty-tree? is-leaf? leaf-val nth-child)
-
-(define (leaf n);leaf-tree
+(define (leaf n)
   (cons 'leaf n))
 
-(define (node l);list -> tree
-  (cons 'tree l))
+(define (node l)
+  (cons 'node l))
+
+(define (is-leaf? t)
+  (if (eq? (car t) 'leaf) #t #f))
+
+(define (leaf-val t)
+  (if (is-leaf? t) (cdr t) #f))
+
+(define (get-nth tl n)
+  (if (null? tl) #f
+      (if (= n 0) (car tl)
+          (get-nth (cdr tl) (- n 1)))))
+
+(define (nth-child t n)
+  (if (eq? (car t) 'node)
+            (get-nth (cdr t) n)
+            #f))
+
+;My code.
+;(define (leaf n);leaf-tree
+  ;(cons 'leaf n))
+
+;(define (node l);list -> tree
+  ;(cons 'tree l))
 
 (define (is-empty-tree? t)
   (null? (cdr t)))
 
-(define (is-leaf? t)
-  (equal? 'leaf (car t)))
+;(define (is-leaf? t)
+  ;(equal? 'leaf (car t)))
 
-(define (is-tree? t)
-  (equal? 'tree (car t)))
+;(define (is-tree? t)
+  ;(equal? 'tree (car t)))
 
-(define (leaf-val t)
-  (cdr t))
+;(define (leaf-val t)
+  ;(cdr t))
 
-(define (nth-child t n)
-  (if (= n 0)
-    (if (is-leaf? t)
-      t
-      (car (cdr t)))
-      (nth-child (cdr t) (- n 1))))
+;(define (nth-child t n)
+  ;(if (= n 0)
+    ;(if (is-leaf? t)
+      ;t
+      ;(car (cdr t)))
+      ;(nth-child (cdr t) (- n 1))))
--- pp/hw1/hw1-6.rkt
+++ pp/hw1/hw1-6.rkt
@@ -34,7 +34,19 @@
 ;use nth-child?
 (define (sub-circuit c n); circuit * int -> circuit
   (if (= n 0)
-    (cond  ((is-not? c) (cdr (leaf-val c)));not is unary op.
+    (cond  ((is-not? c) (leaf-val (cdr c)));not is unary op.
            (else (nth-child (cdr c) 0))); and, or is binary op.
-    (nth-child (cdr (cdr c)) (- n 1)) ))
+    (nth-child (cdr c) n) ))
 
+(define c1 zero)
+(define c2 one)
+(define c3 (not-c c1))
+(define c4 (and-c c2 c3))
+(define c5 (not-c c4))
+(define c6 (or-c c3 c5))
+;(cdr c3)
+;(leaf-val (cdr c3))
+;(sub-circuit c3 0)
+c6
+(sub-circuit c6 0)
+(sub-circuit c6 1)
