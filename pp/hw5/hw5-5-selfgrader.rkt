64f783e5a008c92566b4f73975a3ef9fd917d324
thoum <indiofish@gmail.com>
2015-11-11 22:28:30 +0900

    DONE

--- pp/hw5/hw5-5-selfgrader.rkt
+++ pp/hw5/hw5-5-selfgrader.rkt
@@ -19,10 +19,30 @@
 (define compound3
   (glue compound1 compound2 (turn compound1 2) (turn compound2 2)))
 
-(define foo (rotate (array-to-tree (tree-to-array compound1))))
-(define bar (rotate compound1))
-(equal (rotate compound1) (rotate (array-to-tree (tree-to-array compound1))))
-(equal (glue foo bar foo bar) (glue bar bar foo foo))
+(define foo (rotate (array-to-tree (tree-to-array compound3))))
+(define bar (rotate compound3))
+;(equal (rotate compound3) (rotate (array-to-tree (tree-to-array compound3))))
+;(equal (glue foo bar foo bar) (glue bar bar foo foo))
+;(size (glue foo bar foo bar))
+;(write-string (pprint basic))
+;(size basic)
+
+(define (all-coord f) ;form -> (coord) list
+  (define width (size f))
+  (permutations width '(0 1 2 3)))
+
+(define (permutations size elements)
+  (if (zero? size)
+      '(())
+      (append-map (lambda (p)
+                    (map (lambda (e)
+                           (cons e p))
+                         elements))
+                  (permutations (sub1 size) elements))))
+
+(define compound4 (glue (rotate compound3) (rotate compound3) compound3 compound3))
+(write-string (pprint compound4))
+;(all-coord compound2)
 
 ;;; beautiful test
 
@@ -35,3 +55,4 @@
 (sgoutput (lambda () (equal? #t (beautiful compound1))))
 (sgoutput (lambda () (equal? #f (beautiful compound2))))
 (sgoutput (lambda () (equal? #t (beautiful compound3))))
+(sgoutput (lambda () (equal? #f (beautiful compound4))))
