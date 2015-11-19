fb25e33aade650146366dab14ada4b2a6e8abaf1
Thoum <indiofish@gmail.com>
2015-11-19 23:59:21 +0900

    finished Sorter.java

--- ds/PA04/PA_04/src/ds/sort/Sorter.java
+++ ds/PA04/PA_04/src/ds/sort/Sorter.java
@@ -1,37 +1,118 @@
 package ds.sort;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.ListIterator;
+import java.util.Collections;
 
 public class Sorter{
+  ArrayList<Integer> array;
+  Random rand;
+  boolean sorted;
+
+  final String ASCEND = "ascend";
+  final String DESCEND = "descend";
+  final String LARGEST = "largest";
+  final String SMALLEST = "smallest";
 	
 	public Sorter(int n){
-		
-		//fill your code
-		
+    array = new ArrayList<>(n);
+    rand = new Random();
+    sorted= false;
 	}
 	
-	public void add(int value){
-
-		//fill your code
-		
+ 
+	public void add(int value)
+  { 
+    array.add(value);
+    sorted = false;
 	}
 	
-	public boolean remove(int value){
+	public boolean remove(int value)
+  {//doesn't change sorted state
 
-		//fill your code
-		
-		return false;
+    //box value to use it as a value, not index
+    return array.remove((Integer) value); 
 	}
 	
-	public void sort(String type){
-
-		//fill your code
-		
+	public void sort(String type)
+  {
+    if (!sorted)
+      quickSort(0,array.size()-1);
+    switch (type)
+    {
+      case ASCEND:
+        sorted = true;
+        break;
+      case DESCEND: //reverse the array.
+        Collections.reverse(array);
+        sorted = false;
+        break;
+    }
+    return;
 	}
 	
-	public void top(int k, String type){
+	public String top(int k, String type){
+    ListIterator<Integer> I;
+    String result = "";
 
-		//fill your code
-		
-	}
+    if (sorted) {
+      switch (type) {
+        case SMALLEST:
+          I = array.listIterator();
+          while(k-- > 0)
+            result += I.next() + " ";
+          break;
+        case LARGEST:
+          I = array.listIterator(array.size());
+          while(k-- > 0)
+            result += I.previous() + " ";
+          break;
+      }
+    }
+    else {
+      sort(ASCEND); //will change sort state
+      return top(k,type);
+    }
+    return result;
+  }
+
+  public String toString()
+  {//return space seperated values
+    String ret = array.toString() //csv surrounded with []
+      .replace(",","") //remove commas
+      .replace("[","") //remove []s
+      .replace("]","")
+      .trim();
+
+    return ret;
+  }
+
+  private void quickSort(int first, int last)
+  {
+    int temp;
+    int f = first;
+    int l = last;
+
+    int pivotIndex = rand.nextInt(last-first + 1) + first;
+    int pivot = array.get(pivotIndex);
+
+    while (f <= l) {
+
+      while (array.get(f) < pivot) f++;
+      while (array.get(l) > pivot) l--;
+
+      if (f <= l){
+        temp = array.get(f);
+        array.set(f, array.get(l));
+        array.set(l,temp);
+
+        f++;
+        l--;
+      }
+    }
+
+    if (f < last) quickSort(f, last);
+    if (first < l) quickSort(first, l);
+  }
+
 }
-
-
