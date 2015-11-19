e2e42d31562beccc6b8308404b0c33a408f11097
Thoum <indiofish@gmail.com>
2015-11-20 00:24:31 +0900

    add upgraded selfgrader

--- pp/hw6/hw6_5_selfgrader.ml
+++ pp/hw6/hw6_5_selfgrader.ml
@@ -1,6 +1,35 @@
 open CommonGrade
 open Hw6_5
 
-(* NOTE that this grader is not complete; any expression equivalent to the constant 0 is OK.  Read the specification. *)
+let rec eval_xy (exp:ae) (env:float list): float =
+  match exp with
+  | CONST i -> (float_of_int i)
+  | VAR "x" -> List.nth env 0
+  | VAR "y" -> List.nth env 1
+  | VAR _ -> List.nth env 2
+  | POWER ("x", i) -> (List.nth env 0)**(float_of_int i)
+  | POWER ("y", i) -> (List.nth env 1)**(float_of_int i)
+  | POWER (_, i) -> (List.nth env 2)**(float_of_int i)
+  | TIMES l -> (match l with
+		| [] -> 1.0
+		| h::t -> (eval_xy h env) *. (eval_xy (TIMES t) env))
+  | SUM l -> (match l with
+		| [] -> 0.0
+		| h::t -> (eval_xy h env) +. (eval_xy (TIMES t) env))
+
+let env1 = [1.0; 2.0; 3.0]
+
 let _ = output (fun () ->
-  (diff (CONST 1) "x") = CONST 0)
+		 (int_of_float (eval_xy (diff (CONST 1) "x") env1)) = 0)
+
+let _ = output (fun () ->
+		 (int_of_float (eval_xy (diff (SUM [POWER("x",2);CONST 1]) "x") env1)) = 2)
+
+let _ = output (fun () ->
+		 (int_of_float (eval_xy (diff (SUM [POWER("y",2);CONST 1]) "y") env1)) = 4)
+
+let _ = output (fun () ->
+		 (int_of_float (eval_xy (diff (SUM [POWER("x",2);POWER("y",3)]) "x") env1)) = 2)
+
+let _ = output (fun () ->
+		 (int_of_float (eval_xy (diff (SUM [POWER("x",2);TIMES [VAR "x";VAR "y"]]) "x") env1)) = 4)
