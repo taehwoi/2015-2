83d3c9b639f0454649886a15619997b69e288db0
thoum <indiofish@gmail.com>
2015-11-24 16:43:30 +0900

    done

---
+++ pp/hw7-skeleton/hw7_2.ml
@@ -0,0 +1,48 @@
+module type SKI = sig
+  type liquid =
+    | S
+    | K
+    | I
+    | V of string (* varible *)
+    | M of liquid * liquid (* mix of two liquids *)
+  val react: liquid -> liquid
+  val pprint: liquid -> string
+end
+
+module SkiLiquid : SKI = struct
+  exception ETODO
+  type liquid =
+    | S
+    | K
+    | I
+    | V of string (* varible *)
+    | M of liquid * liquid (* mix of two liquids *)
+
+  let rec react: liquid -> liquid =
+    let rec helper (l: liquid) : liquid =
+      match l with
+      | M (I,e) -> e
+      | M ((M (K,e0)), e1) -> e0
+      | M ((M ((M (S,e0)), e1)), e2) -> (M ((M (e0, e2)), (M (e1, e2))))
+      | M (a,b) -> M ((helper a), (helper b))
+      | _ -> l in
+    fun l ->
+      if l = helper l 
+      then l
+      else react (helper l)
+
+  let rec pprint: liquid -> string =
+    fun l ->
+      match l with
+      | S -> "S"
+      | K -> "K"
+      | I -> "I"
+      | V var -> var
+      | M (l0,l1) -> "(" ^ (pprint l0) ^" "^ (pprint l1) ^ ")" 
+
+let _ = print_endline (pprint (react 
+  (M (M ((M (S,K)), I), V "x"))
+))
+
+
+end
