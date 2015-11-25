ae3807de57141f05ba72037e199fceb695a70e10
thoum <indiofish@gmail.com>
2015-11-25 15:47:02 +0900

    WIP

--- pp/hw7-skeleton/hw7-1.rkt
+++ pp/hw7-skeleton/hw7-1.rkt
@@ -5,9 +5,27 @@
 ; Use the provided functions (inl, inr, case-sum) only.
 
 (provide bstree-make bstree-add-elmt bstree-del-elmt bstree-find-elmt)
+(define null 'null)
+
+; tree should look like this:
+; ((k,v) (left tree) (right tree))
+(define bstree '())
+(define (get-left s)
+  (case-sum
+   (lambda (v) v)
+   (lambda (v) 'not-left)
+   s))
 
 (define (bstree-make)
-  'TODO)
+  (set! bstree 
+    (mcons '() (mcons (inl null) (inr null)))))
+
+(define (left-child node) ;node -> tree
+  (mcar (mcdr node))
+  )
+(define (right-child node) ;node -> tree
+  (mcdr (mcdr node))
+  )
 
 (define (bstree-add-elmt t k v)
   'TODO)
@@ -17,3 +35,8 @@
 
 (define (bstree-find-elmt t k)
   'TODO)
+
+(bstree-make)
+bstree
+(right-child bstree)
+;test -> set bstree's right element to 3
