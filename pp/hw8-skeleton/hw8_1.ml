21bacae31f3a5c30b747a007529cade4aa1ef0db
thoum <indiofish@gmail.com>
2015-11-26 14:40:24 +0900

    finished StringQ

--- pp/hw8-skeleton/hw8_1.ml
+++ pp/hw8-skeleton/hw8_1.ml
@@ -10,10 +10,30 @@ end
 
 module StringQ : Queue with type element = string = 
 struct 
-  ...
+  exception EMPTY_Q
+  type element = string
+  type queue = 
+    | Q of string list * string list
+
+  let emptyq = Q ([], [])
+
+  let enq: queue * element -> queue =
+    fun x -> 
+      match x with
+      | (Q (l0, l1), e) -> Q (e::l0, l1)
+
+  let  deq: queue -> element * queue =
+    fun q -> 
+      match q with
+      | Q (l0, l1) -> 
+        (match l1 with 
+        | [] -> raise EMPTY_Q
+        | h::t -> 
+            if (List.length t = 1) 
+            then (h, Q ([], List.rev l0))
+            else (h, Q (l0, t)))
 end
 
-module StringQQ : Queue with type element = StringQ.queue = 
-struct 
-  ...
-end
+(*module StringQQ : Queue with type element = StringQ.queue = *)
+(*struct *)
+(*end*)
