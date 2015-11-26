18bc8b5b4f94c446a88d862439cce2f74a0c5a60
thoum <indiofish@gmail.com>
2015-11-26 18:31:24 +0900

    finished BasicFrame

--- pp/hw8-skeleton/hw8_4.ml
+++ pp/hw8-skeleton/hw8_4.ml
@@ -14,8 +14,14 @@ module BasicFrame (Design: sig val design: design end): FRAME =
 struct 
   exception NON_BASIC_BOX
   let box = BOX (NW, Design.design)     (* a box is defined *)
-  let rec rotate (b:box) : box = ...
 
+  let rec rotate (b:box) : box = 
+    match b with
+    | BOX (NW, d)-> BOX (NE, d)
+    | BOX (NE, d)-> BOX (SE, d)
+    | BOX (SE, d)-> BOX (SW, d)
+    | BOX (SW, d)-> BOX (NW, d)
+    | _ -> raise NON_BASIC_BOX
 
   let pp b center = 
     match b with
@@ -27,16 +33,16 @@ struct
   let size = 1
 end
 
-module Rotate (Box:FRAME) : FRAME =
-struct 
-  let box = ...
-  let rotate (b:box) : box = ...
-  let pp b center = ()                  (* dummy, fill it if you want *)
-  let size = ...
-end
+(*module Rotate (Box:FRAME) : FRAME =*)
+(*struct *)
+  (*let box = ...*)
+  (*let rotate (b:box) : box = ...*)
+  (*let pp b center = ()                  [> dummy, fill it if you want <]*)
+  (*let size = ...*)
+(*end*)
 
-module Glue (Nw:FRAME) (Ne:FRAME) (Se:FRAME) (Sw:FRAME) : FRAME =
-struct
-  exception DIFFERENT_SIZED_BOXES
-  ...
-end
+(*module Glue (Nw:FRAME) (Ne:FRAME) (Se:FRAME) (Sw:FRAME) : FRAME =*)
+(*struct*)
+  (*exception DIFFERENT_SIZED_BOXES*)
+  (*...*)
+(*end*)
