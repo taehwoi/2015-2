6468888feeee20c06707f64cffcaaad7f301fe32
thoum <indiofish@gmail.com>
2015-11-26 17:12:50 +0900

    finished StringQQ

--- pp/hw8-skeleton/hw8_1.ml
+++ pp/hw8-skeleton/hw8_1.ml
@@ -13,16 +13,15 @@ struct
   exception EMPTY_Q
   type element = string
   type queue = 
-    | Q of string list * string list
+    Q of string list * string list
 
   let emptyq = Q ([], [])
 
   let enq: queue * element -> queue =
     fun x -> 
       match x with
-      | (Q (l0, l1), e) -> Q (e::l0, l1)
+      (Q (l0, l1), e) -> Q (e::l0, l1)
 
-  (*FIXME: refactor!*)
   let rec deq: queue -> element * queue =
     fun q -> 
       match q with
@@ -32,7 +31,26 @@ struct
 
 end
 
-(*module StringQQ : Queue with type element = StringQ.queue = *)
-(*struct *)
-(*end*)
+module StringQQ : Queue with type element = StringQ.queue = 
+struct 
+  exception EMPTY_Q
+  type element = StringQ.queue
+
+  type queue = 
+     Q of StringQ.queue list * StringQ.queue list
+
+  let emptyq = Q ([], [])
+
+  let enq: queue * element -> queue =
+    fun x -> 
+      match x with
+      (Q (l0, l1), e) -> Q (e::l0, l1)
+
+  let rec deq: queue -> element * queue =
+    fun q -> 
+      match q with
+      | Q ([], []) -> raise EMPTY_Q
+      | Q (l0, []) -> deq (Q ([], List.rev l0))
+      | Q (l0, h::t) -> (h, Q (l0, t))
+end
 
