bcf7955254c140a663fc7fa5495c4af7c74d69d4
thoum <indiofish@gmail.com>
2015-11-26 14:40:53 +0900

    added grade script

---
+++ pp/hw8-skeleton/grade.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+elif [ $1 -lt 1 ]; then
+  echo 1>&2 "$0: homework starts from 1"
+else
+  if [[ $# -eq 2 ]]; then
+    x=$2
+  else
+    x=$1
+  fi
+  ocamlc commonGrade.ml
+  for ((i=$1;i<=x;i++))
+  do 
+    if [ ! -f ./hw*_"$i".ml ]; then
+      echo "File not found!"
+    else
+      ocamlc hw*_"$i".ml
+      ocaml commonGrade.cmo hw*_"$i".cmo hw*_"$i"_selfgrader.ml
+      rm -rf hw*_"$i".cm{o,i} a.out
+    fi
+  done
+fi
