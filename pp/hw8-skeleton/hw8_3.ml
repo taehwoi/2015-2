d8da5e595b8e30745182f365db06735526dc0770
thoum <indiofish@gmail.com>
2015-11-26 17:56:54 +0900

    done

--- pp/hw8-skeleton/hw8_3.ml
+++ pp/hw8-skeleton/hw8_3.ml
@@ -17,5 +17,27 @@ end
 module QueueMake (Arg: ArgTy) 
   : Queue with type element = Arg.t =
 struct
-  ...
+  exception EMPTY_Q
+  type element = Arg.t
+  type queue = 
+    Q of element list * element list
+  let emptyq = Q ([], [])
+
+  let enq: queue * element -> queue =
+    fun x -> 
+      match x with
+      (Q (l0, l1), e) -> 
+          Q (e::l0, l1)
+
+  let rec deq: queue -> element * queue =
+    fun q -> 
+      match q with
+      | Q ([], []) -> raise EMPTY_Q
+      | Q (l0, []) -> deq (Q ([], List.rev l0))
+      | Q (l0, h::t) -> (h, Q (l0, t))
 end
+
+(*if ( (List.fold_left (||) false (List.map (fun x -> Arg.is_eq e x) l0))*)
+(*|| (List.fold_left (||) false (List.map (fun x -> Arg.is_eq e x) l1)))*)
+(*then Q (l0, l1)*)
+(*else *)
