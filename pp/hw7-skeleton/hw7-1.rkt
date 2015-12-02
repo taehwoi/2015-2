fc9b32bc3c9c8133de1a1f8d3281f2ed06d0be6c
thoum <indiofish@gmail.com>
2015-12-03 03:10:21 +0900

    append '!' to functions that change state

--- pp/hw7-skeleton/hw7-1.rkt
+++ pp/hw7-skeleton/hw7-1.rkt
@@ -38,31 +38,32 @@
   (if (equal? t null)
     null
     (mcdr (mcdr t))))
-(define (set-left-tree t lt)
-  (set-mcar! (mcdr t) lt))
-(define (set-right-tree t rt)
-  (set-mcdr! (mcdr t) rt))
 
-(define (set-node t node)
+(define (set-left-tree! t lt)
+  (set-mcar! (mcdr t) lt))
+(define (set-right-tree! t rt)
+  (set-mcdr! (mcdr t) rt))
+(define (set-node! t node)
   (set-mcar! t node))
 
 (define (is-leaf? t)
   (and (equal? null (node-key (left-tree t)))
        (equal? null (node-key (right-tree t)))))
+
 (define (bstree-add-elmt t k v)
   (cond
-    ((equal? null (node-key t)) (let () (set-node t (make-node k v)) #f))
-    ((equal? k (node-key t)) (let () (set-node t (make-node k v)) #t))
+    ((equal? null (node-key t)) (let () (set-node! t (make-node k v)) #f))
+    ((equal? k (node-key t)) (let () (set-node! t (make-node k v)) #t))
     ((< k (node-key t))
       (let ()
         (if (equal? (left-tree t) null)
-          (let () (set-left-tree t (make-a-tree k v)) #f)
+          (let () (set-left-tree! t (make-a-tree k v)) #f)
           (bstree-add-elmt (left-tree t) k v))))
     (else 
       (let ()
         (if (equal? (right-tree t) null)
           (let ()
-            (set-right-tree t (make-a-tree k v)) #f)
+            (set-right-tree! t (make-a-tree k v)) #f)
           (bstree-add-elmt (right-tree t) k v))))))
 
 (define (bstree-find-elmt t k)
@@ -84,21 +85,21 @@
     ((equal? k (node-key t)) 
       (let () 
         (cond  
-          ((is-leaf? t) (set-node t null)) ;if leaf tree - just remove tree
+          ((is-leaf? t) (set-node! t null)) ;if leaf tree - just remove tree
           ((equal? null (right-tree t))
            (let ()
-             (set-node t (node (left-tree t)))
-             (set-right-tree t (right-tree (left-tree t)))
-             (set-left-tree t (left-tree (left-tree t)))))
+             (set-node! t (node (left-tree t)))
+             (set-right-tree! t (right-tree (left-tree t)))
+             (set-left-tree! t (left-tree (left-tree t)))))
           ((equal? null (left-tree t)) ;can merge with else, but keep it to make the tree pretty
            (let ()
-             (set-node t (node (right-tree t)))
-             (set-left-tree t (left-tree (right-tree t)))
-             (set-right-tree t (right-tree (right-tree t)))))
+             (set-node! t (node (right-tree t)))
+             (set-left-tree! t (left-tree (right-tree t)))
+             (set-right-tree! t (right-tree (right-tree t)))))
           (else 
             (let ()
               (define min-tree (get-min (right-tree t)))
-              (set-node t (node min-tree))
+              (set-node! t (node min-tree))
               (bstree-del-elmt (right-tree t) (node-key min-tree))
               ))) #t))
     ((< k (node-key t)) (bstree-del-elmt (left-tree t) k))
