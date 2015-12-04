b185e1400a6d6945b4d2f7a2cd622ebd48852f47
thoum <indiofish@gmail.com>
2015-12-04 22:33:16 +0900

    added Probe class

--- ds/PA05/PA_05/src/ds/hash/HashTable.java
+++ ds/PA05/PA_05/src/ds/hash/HashTable.java
@@ -1,25 +1,55 @@
 package ds.hash;
 
-public class HashTable {
+public class HashTable 
+{
+  int[] ht;
 		
-	public HashTable(int n) {
-		
-		// fill your code
-	}
+  public HashTable(int n) {
+    ht = new int[n];
+    // fill your code
+  }
 
-	public void Start(String policy, int constant1, int constant2, int constant3){
-		
-		// fill your code
-	}
-	
-	public void Insert(int value) {
-		
-		// fill your code
-	}
+  public void Start(String policy, int c1, int c2, int c3){
 
-	public int find(int value) {
-		
-		// fill your code
-		return -1;
-	}
+    // fill your code
+  }
+
+  public void Insert(int value) {
+
+    // fill your code
+  }
+
+  public int find(int value) {
+
+    // fill your code
+    return -1;
+  }
+
+
+  private class Probe
+  {
+    int c1, c2, c3;
+    String policy;
+    public Probe(String policy, int c1, int c2, int c3)
+    {
+      this.c1 = c1;
+      this.c2 = c2;
+      this.c3 = c3;
+      this.policy = policy;
+    }
+
+    public int p(int i)
+    {
+      int val = 0;
+      switch (policy) {
+        case "linear" :
+          val =  c1 * i;
+          break;
+        case "quadratic" :
+          val = (c1 * i * i) + (c2 * i) + c3;
+          break;
+      }     
+      return val;
+    }
+  }
 }
