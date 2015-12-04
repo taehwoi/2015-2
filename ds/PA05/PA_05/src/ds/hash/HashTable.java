56582154275749a7bc4693c7c5c2f010a102397f
thoum <indiofish@gmail.com>
2015-12-05 02:29:38 +0900

    finished HT

--- ds/PA05/PA_05/src/ds/hash/HashTable.java
+++ ds/PA05/PA_05/src/ds/hash/HashTable.java
@@ -1,30 +1,49 @@
 package ds.hash;
-
+//(h(K) + p(k,i)) mod M
 public class HashTable 
 {
   int[] ht;
+  private int M;
+  Probe probe;
+  
 		
   public HashTable(int n) {
-    ht = new int[n];
-    // fill your code
+    M = n;
+    ht = new int[M];
   }
 
   public void Start(String policy, int c1, int c2, int c3){
-
-    // fill your code
+    probe = new Probe(policy, c1, c2, c3);
   }
 
   public void Insert(int value) {
-
-    // fill your code
+    int key=0;
+    for (int i=0;;i++) {
+      key = (h(value) + probe.p(i)) % M;
+      if (ht[key] == (Integer) null) {
+        ht[key] = value;
+        break;
+      }
+    }
   }
 
   public int find(int value) {
-
-    // fill your code
+    int key=0;
+    for (int i=0;;i++) {
+      key = (h(value) + probe.p(i)) % M;
+      if (ht[key] == value) 
+        return key;
+      else if (ht[key] == (Integer) null) {
+        break;
+      }
+    }
     return -1;
   }
 
+  private int h(int K)
+  {
+    return K % M;
+  }
 
   private class Probe
   {
